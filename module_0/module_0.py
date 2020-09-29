8b6eeb0da926c80147ff7ef613b4a736d27dcb0c
mikolya7 <38215586+mikolya7@users.noreply.github.com>
2020-09-29 16:45:14 +0300

    Update module_0.py
    незначительные улучшения

--- module_0/module_0.py
+++ module_0/module_0.py
@@ -23,7 +23,7 @@ def game_core_v2(number, from_value=1, max_value=100):
         elif number < predict:
             max_value = predict - 1
         else:
-            found = True  # угадали число number, завершаем цилк
+            found = True  # угадали число number, завершаем цикл
 
     return predictions_count
 
@@ -32,12 +32,12 @@ def score_game(game_core):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(1000))
+    random_array = np.random.randint(1, 101, size=1000)
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return (score)
+    return score
 
 
 # Проверяем
